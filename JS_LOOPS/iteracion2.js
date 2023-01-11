ec996c6baee1c99c8528e1b4c3f7301d05a2389d
Charliecgs <cgsmanu@gmail.com>
2023-01-11 15:40:30 +0100

    iteracion2 loops

--- JS_LOOPS/iteracion2.js
+++ JS_LOOPS/iteracion2.js
@@ -1,6 +1,7 @@
 //Iteración #2: Condicionales avanzados
 
-/*Comprueba en cada uno de los usuarios que tenga al menos dos trimestres aprobados y añade la propiedad ***isApproved*** a true o false en consecuencia. Una vez lo tengas compruébalo con un ***console.log***. 
+/*Comprueba en cada uno de los usuarios que tenga al menos dos trimestres aprobados 
+y añade la propiedad ***isApproved*** a true o false en consecuencia. Una vez lo tengas compruébalo con un ***console.log***. 
 
 ( **Mirar abajo en pistas** ).
 
@@ -13,3 +14,12 @@ const alumns = [
 	{name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
 	{name: 'Raquel Benito', T1: true, T2: true, T3: true}
 ]
+
+for (let i = 0; i < alumns.length; i++) {
+	if (alumns[i].T1 == true && alumns[i].T2 == true || alumns[i].T1 == true && alumns[i].T3 == true || alumns[i].T2 == true && alumns[i].T3 == true){
+		alumns[i].isApproved = true;
+	} else {
+		alumns[i].isApproved = false;
+	}
+	console.log(alumns[i]);
+}
