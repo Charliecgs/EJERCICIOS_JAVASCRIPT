e5b662fb093e359612acc59fac1113f904e40e2d
Charliecgs <cgsmanu@gmail.com>
2023-01-13 16:32:31 +0100

    iteracion7 functions

--- JS_FUNCTIONS/iteracion6.js
+++ JS_FUNCTIONS/iteracion6.js
@@ -2,28 +2,27 @@
 
 // Crea una función que reciba por parámetro un array y compruebe si existen elementos duplicados, en caso que existan los elimina para retornar un array sin los elementos duplicados. Puedes usar este array para probar tu función:
 const duplicates = [
-    'sushi',
-    'pizza',
-    'burger',
-    'potatoe',
-    'pasta',
-    'ice-cream',
-    'pizza',
-    'chicken',
-    'onion rings',
-    'pasta',
-    'soda'
-  ];
+  'sushi',
+  'pizza',
+  'burger',
+  'potatoe',
+  'pasta',
+  'ice-cream',
+  'pizza',
+  'chicken',
+  'onion rings',
+  'pasta',
+  'soda'
+];
 
-  function removeDuplicates(param) {
-    let noDuplicates = [];
-    param.forEach(element => {
-        if(!noDuplicates.includes(element)){
-            noDuplicates.push(element);
-        }
-    });
-    return noDuplicates;
-  }
+function removeDuplicates(param) {
+  let noDuplicates = [];
+  param.forEach(element => {
+      if(!noDuplicates.includes(element)){
+          noDuplicates.push(element);
+      }
+  });
+  return noDuplicates;
+}
 
 console.log(removeDuplicates(duplicates));
-
---
+++ JS_FUNCTIONS/iteracion8.js
@@ -0,0 +1,24 @@
+//Iteration #8: Contador de repeticiones
+
+//Crea una función que nos devuelva el número de veces que se repite cada una de las palabras que lo conforma.  Puedes usar este array para probar tu función:
+
+//ESTE ESTA MAL TODAVIA NO LO HE HECHO
+
+const counterWords = [
+  "code",
+  "repeat",
+  "eat",
+  "sleep",
+  "code",
+  "enjoy",
+  "sleep",
+  "code",
+  "enjoy",
+  "upgrade",
+  "code",
+];
+function repeatCounter(param) {
+  param.forEach(element => {
+    
+  });
+}
