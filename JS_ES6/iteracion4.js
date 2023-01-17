a55397219caf74d4f19f9cc0466cb56533ed528d
Charliecgs <cgsmanu@gmail.com>
2023-01-17 16:59:06 +0100

    iteracion4 es6

--- JS_ES6/iteracion4.js
+++ JS_ES6/iteracion4.js
@@ -5,14 +5,14 @@
 utilizando .map().
 */
 const users = [
-	{id: 1, name: 'Abel'},
-	{id:2, name: 'Julia'},
-	{id:3, name: 'Pedro'},
-	{id:4, name: 'Amanda'}
+  { id: 1, name: "Abel" },
+  { id: 2, name: "Julia" },
+  { id: 3, name: "Pedro" },
+  { id: 4, name: "Amanda" },
 ];
 
-const nombres = users.map((userName) =>({
-    name: userName.name
+const nombres = users.map((userName) => ({
+  name: userName.name,
 }));
 
 console.log(nombres);
@@ -22,18 +22,43 @@ console.log(nombres);
 de la propiedad .name y cambia el nombre a 'Anacleto' en caso de que 
 empiece por 'A'.
 */
- const users1 = [
- 	{id: 1, name: 'Abel'},
- 	{id:2, name: 'Julia'},
- 	{id:3, name: 'Pedro'},
- 	{id:4, name: 'Amanda'} 
+const users1 = [
+  { id: 1, name: "Abel" },
+  { id: 2, name: "Julia" },
+  { id: 3, name: "Pedro" },
+  { id: 4, name: "Amanda" },
 ];
 
 const users2 = users1.map((user) => user.name);
 
+for (let i = 0; i < users2.length; i++) {
+	if(users2[i][0].includes("A")){
+		users2[i] = "Anacleto";
+	}
+}
+
 console.log(users2);
 
+/*
+4.3 Dado el siguiente array, devuelve una lista que contenga los valores 
+de la propiedad .name y añade al valor de .name el string ' (Visitado)' 
+cuando el valor de la propiedad isVisited = true.
+*/
+const cities = [
+	{isVisited:true, name: 'Tokyo'}, 
+	{isVisited:false, name: 'Madagascar'},
+	{isVisited:true, name: 'Amsterdam'}, 
+	{isVisited:false, name: 'Seul'}
+];
 
-
-
+for (let i = 0; i < cities.length; i++) {
+  if(cities[i].isVisited == true){
+    cities[i].name = cities[i].name + " visitado";
+  }
   
+}
+
+const visited = cities.map((city) => city.name);
+
+console.log(visited);
+
