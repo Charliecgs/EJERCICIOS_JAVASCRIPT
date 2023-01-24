2b5041017a80f37e3f0b4ebbb98bcaf321a987df
Charliecgs <cgsmanu@gmail.com>
2023-01-24 14:36:22 +0100

    iteracones 7,8 es6

--- JS_ES6/iteracion7.js
+++ JS_ES6/iteracion7.js
@@ -4,20 +4,72 @@
 7.1 Dado el siguiente array, haz una suma de todos las notas de los examenes de 
 los alumnos usando la función .reduce().
 */
-
 const exams = [
-    {name: 'Yuyu Cabeza Crack', score: 5}, 
-    {name: 'Maria Aranda Jimenez', score: 1}, 
-    {name: 'Cristóbal Martínez Lorenzo', score: 6}, 
-    {name: 'Mercedez Regrera Brito', score: 7},
-    {name: 'Pamela Anderson', score: 3},
-    {name: 'Enrique Perez Lijó', score: 6},
-    {name: 'Pedro Benitez Pacheco', score: 8},
-    {name: 'Ayumi Hamasaki', score: 4},
-    {name: 'Robert Kiyosaki', score: 2},
-    {name: 'Keanu Reeves', score: 10}
+  { name: "Yuyu Cabeza Crack", score: 5 },
+  { name: "Maria Aranda Jimenez", score: 1 },
+  { name: "Cristóbal Martínez Lorenzo", score: 6 },
+  { name: "Mercedez Regrera Brito", score: 7 },
+  { name: "Pamela Anderson", score: 3 },
+  { name: "Enrique Perez Lijó", score: 6 },
+  { name: "Pedro Benitez Pacheco", score: 8 },
+  { name: "Ayumi Hamasaki", score: 4 },
+  { name: "Robert Kiyosaki", score: 2 },
+  { name: "Keanu Reeves", score: 10 },
 ];
 
 const suma = exams.reduce((acc, exam) => acc + exam.score, 0);
 
 console.log(suma);
+
+// 7.2 Dado el mismo array, haz una suma de todos las notas de los examenes de los
+// alumnos que esten aprobados usando la función .reduce().
+const exams2 = [
+  { name: "Yuyu Cabeza Crack", score: 5 },
+  { name: "Maria Aranda Jimenez", score: 1 },
+  { name: "Cristóbal Martínez Lorenzo", score: 6 },
+  { name: "Mercedez Regrera Brito", score: 7 },
+  { name: "Pamela Anderson", score: 3 },
+  { name: "Enrique Perez Lijó", score: 6 },
+  { name: "Pedro Benitez Pacheco", score: 8 },
+  { name: "Ayumi Hamasaki", score: 4 },
+  { name: "Robert Kiyosaki", score: 2 },
+  { name: "Keanu Reeves", score: 10 },
+];
+
+let aprobados = [];
+
+for (let i = 0; i < exams2.length; i++) {
+  if (exams2[i].score >= 5) {
+    aprobados.push(exams2[i].score);
+  }
+}
+
+const suma2 = aprobados.reduce((acc, note) => acc + note, 0);
+
+console.log(suma2);
+
+// 7.3 Dado el mismo array, haz la media de las notas de todos los examenes .reduce().
+const exams3 = [
+  { name: "Yuyu Cabeza Crack", score: 5 },
+  { name: "Maria Aranda Jimenez", score: 1 },
+  { name: "Cristóbal Martínez Lorenzo", score: 6 },
+  { name: "Mercedez Regrera Brito", score: 7 },
+  { name: "Pamela Anderson", score: 3 },
+  { name: "Enrique Perez Lijó", score: 6 },
+  { name: "Pedro Benitez Pacheco", score: 8 },
+  { name: "Ayumi Hamasaki", score: 4 },
+  { name: "Robert Kiyosaki", score: 2 },
+  { name: "Keanu Reeves", score: 10 },
+];
+
+const media = exams3.reduce((acc, exam) => acc + exam.score, 0);
+
+let totalExamns = [];
+
+for (let i = 0; i < exams3.length; i++) {
+  totalExamns.push(exams[i].score);
+}
+
+let mediaFinal = media / totalExamns.length;
+
+console.log(mediaFinal);
---
+++ JS_ES6/iteracion8.js
@@ -0,0 +1,32 @@
+// Iteración #8: Bonus
+
+// 8.1 Dado el siguiente javascript filtra los videojuegos por gender = 'RPG' usando
+// .filter() y usa .reduce() para conseguir la media de sus .score.
+// La función .find() también podría ayudarte para el contrar el genero 'RPG' en el
+// array .gender.
+const videogames = [
+  { name: "Final Fantasy VII", genders: ["RPG"], score: 9.5 },
+  { name: "Assasins Creed Valhala", genders: ["Aventura", "RPG"], score: 4.5 },
+  { name: "The last of Us 2", genders: ["Acción", "Aventura"], score: 9.8 },
+  { name: "Super Mario Bros", genders: ["Plataforma"], score: 8.5 },
+  { name: "Genshin Impact", genders: ["RPG", "Aventura"], score: 7.5 },
+  {
+    name: "Legend of Zelda: Breath of the wild",
+    genders: ["RPG", "La cosa más puto bonita que he visto nunca"],
+    score: 10,
+  },
+];
+
+const filtrados = videogames.filter((game) => game.genders.includes("RPG"));
+
+let medias = [];
+
+for (let i = 0; i < filtrados.length; i++) {
+    medias.push(filtrados[i].score);
+}
+
+const mediasFinales = medias.reduce((acc, note) => acc + note, 0);
+
+let mediaFinal = mediasFinales / medias.length;
+
+console.log(mediaFinal);
