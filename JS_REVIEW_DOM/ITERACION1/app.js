fb68e02514bcacacaf1438a0920c33f8f8d43d6a
Charliecgs <cgsmanu@gmail.com>
2023-01-30 10:59:10 +0100

    iteracion1 review dom

--- JS_REVIEW_DOM/ITERACION1/app.js
+++ JS_REVIEW_DOM/ITERACION1/app.js
@@ -20,6 +20,8 @@ for (const item of element) {
   item.remove();
 }
 
+console.log(element);
+
 // 1.3 Utiliza el array para crear dinamicamente una lista ul > li de elementos
 // en el div de html con el atributo data-function="printHere".
 const cars = ["Mazda 6", "Ford fiesta", "Audi A4", "Toyota corola"];
@@ -50,7 +52,7 @@ const countries2 = [
 const uls3 = document.createElement("ul");
 
 for (const item of countries2) {
-  const lis3 = document.createElement("li");
+  const lis3 = document.createElement("div");
   uls3.appendChild(lis3);
 
   const titles = document.createElement("h4");
@@ -58,13 +60,91 @@ for (const item of countries2) {
   titles.appendChild(titleContent);
   lis3.appendChild(titles);
 
-  const images = document.createElement("img");
-  const imagesContent = document.createTextNode(item.imgUrl);
-  images.appendChild(imagesContent);
-  lis3.appendChild(images);
+  lis3.innerHTML += ` 
+  <img src=${item.imgUrl} alt=${item.title}/>
+  `; 
 }
 
 document.body.appendChild(uls3);
 
 // 1.5 Basandote en el ejercicio anterior. Crea un botón que elimine el último
 // elemento de la lista.
+
+const uls4 = document.createElement("ul");
+
+for (const item of countries2) {
+  const lis3 = document.createElement("div");
+  uls4.appendChild(lis3);
+
+  const titles = document.createElement("h4");
+  const titleContent = document.createTextNode(item.title);
+  titles.appendChild(titleContent);
+  lis3.appendChild(titles);
+
+  lis3.innerHTML += ` 
+  <img src=${item.imgUrl} alt=${item.title}/>
+  `; 
+  lis3.classList.add("eliminar");
+}
+
+document.body.appendChild(uls4);
+
+const lis5 = document.querySelectorAll(".eliminar");
+console.log(lis5);
+
+const creaBoton = document.createElement("button");
+
+document.body.appendChild(creaBoton);
+
+creaBoton.classList.add("boton");
+
+creaBoton.textContent = "Pulsa para eliminar el ultimo elemento";
+
+const botonPulsado = document.querySelector(".boton");
+
+const eliminarElUltimo = () => {
+  uls4.removeChild(uls4.lastChild);
+}
+
+
+botonPulsado.addEventListener("click", () => {
+  eliminarElUltimo();
+});
+
+
+// 1.6 Basandote en el ejercicio anterior. Crea un botón para cada uno de los 
+// elementos de las listas que elimine ese mismo elemento del html.
+
+const uls5 = document.createElement("ul");
+
+for (const item of countries2) {
+  const lis3 = document.createElement("div");
+  uls5.appendChild(lis3);
+
+  const titles = document.createElement("h4");
+  const titleContent = document.createTextNode(item.title);
+  titles.appendChild(titleContent);
+  lis3.appendChild(titles);
+
+  lis3.innerHTML += ` 
+  <img src=${item.imgUrl} alt=${item.title}/>
+  `; 
+
+  const botonDeCadaUno = document.createElement("button");
+  botonDeCadaUno.classList.add("botonParaEliminar")
+  lis3.appendChild(botonDeCadaUno);
+  botonDeCadaUno.textContent = "Pulsa para eliminar";
+  lis3.classList.add("ulClass");
+
+  botonDeCadaUno.addEventListener("click", () => {
+    lis3.remove();
+  })
+}
+
+document.body.appendChild(uls5);
+
+
+
+
+
+
